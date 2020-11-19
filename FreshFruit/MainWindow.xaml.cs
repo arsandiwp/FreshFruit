9365f477579e73a53b2ac9240b0c0d990b908b7c
arsandiwp <58413833+arsandiwp@users.noreply.github.com>
2020-11-19 13:31:36 +0700

    Add files via upload

---
+++ FreshFruit/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using FreshFruit.Controller;
+using FreshFruit.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FreshFruit
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, BucketEventListener
+    {
+        Seller Arsandi;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Bucket keranjangBuah = new Bucket(2);
+            BucketController bucketController = new BucketController(keranjangBuah, this);
+
+            Arsandi = new Seller("Arsandi", bucketController);
+
+            listBoxBucket.ItemsSource = keranjangBuah.findAll();
+        }
+
+        public void onFailed(string message)
+        {
+            MessageBox.Show(message, "Warning");
+        }
+
+        public void onSucceed(string message)
+        {
+            listBoxBucket.Items.Refresh();
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            Fruit Anggur = new Fruit("Anggur");
+            Arsandi.AddFruit(Anggur);
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            Fruit Apel = new Fruit("Apel");
+            Arsandi.AddFruit(Apel);
+        }
+
+        private void Button3_Click(object sender, RoutedEventArgs e)
+        {
+            Fruit Pisang = new Fruit("Pisang");
+            Arsandi.AddFruit(Pisang);
+        }
+
+        private void Button4_Click(object sender, RoutedEventArgs e)
+        {
+            Fruit Jeruk = new Fruit("Jeruk");
+            Arsandi.AddFruit(Jeruk);
+        }
+    }
+}
---
+++ FreshFruit/model/Seller.cs
@@ -0,0 +1,31 @@
+﻿using FreshFruit.Controller;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreshFruit.model
+{
+    class Seller
+    {
+        private string name;
+        private BucketController bucket;
+
+        public Seller(string name, BucketController bucket)
+        {
+            this.name = name;
+            this.bucket = bucket;
+        }
+
+        public List<Fruit> findAll()
+        {
+            return this.bucket.findAll();
+        }
+
+        public void AddFruit(Fruit fruit)
+        {
+            this.bucket.addFruit(fruit);
+        }
+    }
+}
